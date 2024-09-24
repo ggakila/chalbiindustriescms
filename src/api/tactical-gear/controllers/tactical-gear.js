b4b6f0fcc409b8935c8087cb40a52eef99c2fd77
ggakila <gisorehaggai@gmail.com>
2024-09-24 17:59:51 +0300

    added more collections

---
+++ src/api/tactical-gear/controllers/tactical-gear.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tactical-gear controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tactical-gear.tactical-gear');
---
+++ src/api/tactical-gear/routes/tactical-gear.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tactical-gear router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::tactical-gear.tactical-gear');
