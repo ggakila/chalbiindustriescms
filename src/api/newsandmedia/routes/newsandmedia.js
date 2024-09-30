7ec79decdcc081103d817173dfd638768fd79715
ggakila <gisorehaggai@gmail.com>
2024-09-30 21:55:53 +0300

    landing page connected with cms

---
+++ src/api/newsandmedia/routes/newsandmedia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newsandmedia router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::newsandmedia.newsandmedia');
---
+++ src/api/newsandmedia/services/newsandmedia.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * newsandmedia service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::newsandmedia.newsandmedia');
